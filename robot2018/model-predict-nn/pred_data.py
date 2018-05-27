0474f1f067ae415e69be1dd1fb4947fa42ed1a2f
Jeremy Feltracco <jpfeltracco@gatech.edu>
2018-05-26 23:11:59 -0400

    More hyperparameter tweaking, not really getting a good fit on the model

--- robot2018/model-predict-nn/pred_data.py
+++ robot2018/model-predict-nn/pred_data.py
@@ -24,10 +24,10 @@ def data_to_nn(df):
     df["ang"] = np.arctan2(np.sin(df["ang"]), np.cos(df["ang"])) / (2*np.pi)
 
     for h in pos_headers:
-        df[h] = df[h] / 4000
+        df[h] = df[h] / 10000
 
     for h in enc_headers:
-        df[h] = df[h] / 3000
+        df[h] = df[h] / 5000
 
     return df
 
@@ -37,7 +37,7 @@ def nn_to_data(np_array):
     pos_headers = ["x", "y"]
 
     for h in pos_headers:
-        df[h] = df[h] * 4000
+        df[h] = df[h] * 10000
 
     #  df["ang"] = df["ang"] * 2 * np.pi
 
@@ -53,22 +53,25 @@ def get_data(frame_delay=6, window_size=3):
     x_data = []
     y_data = []
 
-    # Data input to LSTM is 3d, need features * window_size * sample_count
-    for i in range(0, len(data) - window_size - frame_delay):
+    y_slice = np.split(data, [3], axis=1)[0]
+
+    # we receive measurement at beginning of each frame
+
+    # can only start predictions once window_size has finished
+    for time_ind in range(0+window_size-1, len(data) - frame_delay):
         log("--------------------------------------------------------------------------------")
-        log("Processing row {}".format(i))
+        #  log("Processing row {}".format(time_ind))
 
-        # go from i-window_size to i
-        x = data.iloc[i:i+window_size]    
-
-        # time is i+window_size
-
-        # grab only x, y, ang fields from the dataframe
-        y = np.split(data, [3], axis=1)[0].iloc[i+window_size+frame_delay]
+        # we have received all measurements up to and including time_ind
+        x = data.iloc[time_ind-window_size+1:time_ind+1]
+        # we just received time_ind, which is a frame_delay version of
+        # current position. We estimate frame delay to be 6 cycles,
+        # so take our time_ind and get 6 frames ahead.
+        y = y_slice.iloc[time_ind + frame_delay]
 
         x_data.append(x)
         y_data.append(y)
-
+        
         log("X data")
         log(x)
         log("Y_data")
